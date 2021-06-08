f9dce8b3e867641128c1f6301ccd5faab70c213a
finkstefan <stefan.fink@gmail.com>
2021-06-08 20:43:57 +0200

    Endpoints

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,9 +1,9 @@
-export const FAKULTET_URL = "http://localhost:8083/fakultet"
+export const FAKULTET_URL = "http://localhost:8084/fakultet"
 
-export const STATUS_URL = "http://localhost:8083/status"
+export const STATUS_URL = "http://localhost:8084/status"
 
-export const DEPARTMAN_URL = "http://localhost:8083/departman"
+export const DEPARTMAN_URL = "http://localhost:8084/departman"
 
-export const STUDENT_URL = "http://localhost:8083/student"
+export const STUDENT_URL = "http://localhost:8084/student"
 
-export const STUDENTI_ZA_DEPARTMAN_URL = "http://localhost:8083/studentByDepartman"
+export const STUDENTI_ZA_DEPARTMAN_URL = "http://localhost:8084/studentByDepartman"
