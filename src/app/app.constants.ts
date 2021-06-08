a69f261f1e4c1f1730bf34284996b7629e891e3e
finkstefan <stefan.fink@gmail.com>
2021-06-08 21:03:31 +0200

    Constants

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,9 +1,10 @@
-export const FAKULTET_URL = "http://localhost:8084/fakultet"
+export const FAKULTET_URL = "https://backendrva2021.herokuapp.com/fakultet"
 
-export const STATUS_URL = "http://localhost:8084/status"
+export const STATUS_URL = "https://backendrva2021.herokuapp.com/status"
 
-export const DEPARTMAN_URL = "http://localhost:8084/departman"
+export const DEPARTMAN_URL = "https://backendrva2021.herokuapp.com/departman"
 
-export const STUDENT_URL = "http://localhost:8084/student"
+export const STUDENT_URL = "https://backendrva2021.herokuapp.com/student"
 
-export const STUDENTI_ZA_DEPARTMAN_URL = "http://localhost:8084/studentByDepartman"
+export const STUDENTI_ZA_DEPARTMAN_URL = "https://backendrva2021.herokuapp.com/studentByDepartman"
+
